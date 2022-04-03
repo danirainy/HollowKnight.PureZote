b46028ff4d493309e194963e4f1d131e8bdb0625
Njord Yu <yunjord@gmail.com>
2022-04-02 22:24:15 -0700

    Updated.

--- Pure Zote/ModClass.cs
+++ Pure Zote/ModClass.cs
@@ -1,20 +1,47 @@
 ﻿using Modding;
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UObject = UnityEngine.Object;
+using Core.FsmUtil;
+
 
 namespace Pure_Zote
 {
     public class Pure_Zote : Mod
     {
-        public override string GetVersion() => "0.1";
-        public override void Initialize()
+        private GameObject minion;
+        public Pure_Zote() : base("Pure Zote") { }
+        public override string GetVersion() => "1.0";
+        public override List<(string, string)> GetPreloadNames()
         {
-            ModHooks.HeroUpdateHook += OnHeroUpdate;
+            return new List<(string, string)>
+            {
+                ("Deepnest_East_07", "Super Spitter")
+            };
         }
-        public void OnHeroUpdate()
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+        {
+            Log("Initializing.");
+            On.PlayMakerFSM.OnEnable += PlayMakerFSMOnEnable;
+            minion = preloadedObjects["Deepnest_East_07"]["Super Spitter"];
+            UnityEngine.SceneManagement.SceneManager.activeSceneChanged += ActiveSceneChanged;
+            ModHooks.HeroUpdateHook += HeroUpdateHook;
+            Log("Initialized.");
+        }
+        private void PlayMakerFSMOnEnable(On.PlayMakerFSM.orig_OnEnable original, PlayMakerFSM fsm)
+        {
+            original(fsm);
+            if (fsm.gameObject.scene.name == "GG_Grey_Prince_Zote" && fsm.gameObject.name == "Grey Prince" && fsm.FsmName == "Control")
+            {
+                Log("Upgrading FSM.");
+                FsmUtil.AddMethod(fsm, "Spit L", () => { Log("Spit L"); });
+                FsmUtil.AddMethod(fsm, "Spit R", () => { Log("Spit R"); });
+                Log("Upgraded FSM.");
+            }
+        }
+        private void ActiveSceneChanged(UnityEngine.SceneManagement.Scene from, UnityEngine.SceneManagement.Scene to)
+        {
+        }
+        private void HeroUpdateHook()
         {
         }
     }
